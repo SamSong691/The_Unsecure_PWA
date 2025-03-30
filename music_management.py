79e276f2fbe6a11f251fc0ab3b248ffb2eb2e85d
Peter Song <petersong61@gmail.com>
2025-03-30 18:49:19 +1100

    Updated music pages for new table schema

--- music_management.py
+++ music_management.py
@@ -1,25 +1,32 @@
 import sqlite3 as sql
 
-def getSongCategory():
-    return ["Pop", "Country"]
-
 def listAll():
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    data = cur.execute("select id,song_name,artist,song_image_filename,category from musics order by id desc").fetchall()
+    data = cur.execute("select id,title,artist,song_image_filename,genre,album,duration from musics order by id desc").fetchall()
     con.close()
     musicList = []
     for row in data:
-        musicList.append(dict(id=row[0], song_name=row[1], artist=row[2], song_image_filename=row[3], category=row[4]))
+        musicList.append(dict(id=row[0], title=row[1], artist=row[2], song_image_filename=row[3], genre=row[4], album=row[5], duration=secondsToStr(row[6])))
     print(musicList)
     return musicList
+
+def secondsToStr(seconds):
+    hours = seconds//3600
+    remain = seconds%3600
+    mins = remain//60
+    secs = remain%60
+    if hours > 0:
+        return f"{hours}:{mins}:{secs}"
+    else:
+        return f"{mins}:{secs}"
 
 def search(key):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    data = cur.execute(f"select id,song_name,artist,song_image_filename,category from musics where song_name like '%{key}%' or artist like '%{key}%' or category like '%{key}%'").fetchall()
+    data = cur.execute(f"select id,title,artist,song_image_filename,genre,album,duration from musics where title like '%{key}%' or artist like '%{key}%' or genre like '%{key}%'").fetchall()
     con.close()
     musicList = []
     for row in data:
-        musicList.append(dict(id=row[0], song_name=row[1], artist=row[2], song_image_filename=row[3], category=row[4]))
+        musicList.append(dict(id=row[0], title=row[1], artist=row[2], song_image_filename=row[3], genre=row[4], album=row[5], duration=secondsToStr(row[6])))
     return musicList
